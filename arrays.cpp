3bca5ac80f95e6478685497f6fbfe75f37f0bd5e
Adelaida Zelaya <zelayadelaida@gmail.com>
2020-02-07 19:28:57 -0600

    Mi primer commit

---
+++ arrays.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+#include <assert.h>
+
+using namespace std;
+
+int calcularNumeroMayor(int arr[]) {
+	return 1;
+}
+
+int main(){
+	int a[]={1,4,3,2,1,7,4,3,9,0,87,64,32,14,60,86};
+
+	int numeroMayor = calcularNumeroMayor(a);
+	
+	
+	 assert(numeroMayor == 87);
+	 system("PAUSE");
+
+	 return 0;
+}
